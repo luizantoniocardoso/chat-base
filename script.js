b50b5010739d2a77e124eae55e9066eee3a3c757
Arthur Romansini Fernandes <arthur.177322@alunosatc.edu.br>
2023-03-02 20:14:48 -0300

    add gitignore

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import KEYS from "./KEYS";
+
 const remetente = `<div class="chatMessageRemetente">`;
 const destinatario = `<div class="chatMessageDestinatario">`;
 
@@ -49,7 +51,7 @@ async function botMessage(mensagem) {
 }
 
 async function postChatGpt3(prompt) {
-  const API_KEY = "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";
+  const API_KEY = KEYS?.API_KEY;
   const API_URL =
     "https://api.openai.com/v1/engines/text-davinci-003/completions";
   const rest = await fetch(API_URL, {
